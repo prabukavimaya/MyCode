369943f34c673f14fb38649cf684f6eb0e8a0e31
Mayandi <prabumaya.m3@gmail.com>
2016-02-07 16:43:11 +0800

    split the files

--- home.php
+++ home.php
@@ -1,43 +1,28 @@
 <?php
-include_once 'config/dbconfig.php';
-if(!$user->is_loggedin())
-{
- $user->redirect('index.php');
-}
-$user_id = $_SESSION['user_session'];
-$stmt = $DB_con->prepare("SELECT * FROM users WHERE user_id=:user_id");
-$stmt->execute(array(":user_id"=>$user_id));
-$userRow=$stmt->fetch(PDO::FETCH_ASSOC);
+	
+/* Author: Mayandi 
+*Date Created: Feb-07-2016
+*home.php
+*/
+	
+
+	/************header***********/
+	include "form/header.php";
+	/************header***********/
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css"  />
-<link rel="stylesheet" href="css/style.css" type="text/css"  />
-<link rel="stylesheet" href="css/menu.css" type="text/css"  />
-<title>welcome - <?php print($userRow['user_email']); ?></title>
-</head>
 
-<body>
+	<!------Content------------------>
+	<div class="content">
+		welcome : <?php print($userRow['user_name']); ?>
+	</div>
+	<!------Content------------------>
 
-<div class="header">
- <div class="left">
-     <label><a href="http://www.codingcage.com/">Admin Panel</a></label>
 
-    </div>
+<?php
+	/************footerr***********/
+	include "form/footer.php";
+	/************footerr***********/
+?>
 
-    <div class="right">
-     <label><a href="logout.php?logout=true"><i class="glyphicon glyphicon-log-out"></i> logout</a></label>
-    </div>
-</div>
-<div class="left" style="font-size:14px;margin-top:0px;margin-left:0px;width:250px">
-<?php include_once "form/menu.php";?>
-</div>
 
-<div class="content">
-welcome : <?php print($userRow['user_name']); ?>
-</div>
 
-</body>
-</html>
