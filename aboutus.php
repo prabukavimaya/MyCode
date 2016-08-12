f8b0e4c81d093083e497d52ad39a7153a98bb367
Mayandi <prabumaya.m317@gmail.com>
2016-08-12 12:56:07 +0800

    content changed

--- aboutus.php
+++ aboutus.php
@@ -33,14 +33,7 @@
 
 		<p> 
 		
-		"Spread Your Wings" is not just a marketing slogan that sounds fun. This slogan represents the core of the Odyssee Mobile philosophy, both Odyssee as a company and for our customers.
-
-		"Spread your Wings" is applicable in several areas. Odyssee Mobile is a fast growing company in an innovative market. Odyssee Mobile is spreading its wings by constantly working and investing in new developments to build top applications in an increasingly evolving platform.
-
-		Your company and your mobile team can spread their wings with the help of Odyssee Mobile. Odyssee solutions provide the ability to elevate your entire company.
-
-		If you equip your field team with Odyssee Mobile applications, you can rest assured as they head out. They have all the tools necessary to carry out their job and you can easily monitor and manage everything.
-		The possibilities are endless for your business!
+		About Us
 
  
 
